ada2e5836653067e9ba2243bfb340c6db7de376a
mevol <melanie.vollmar@diamond.ac.uk>
2018-07-20 10:35:42 +0100

    now writing the different training and testsets as well as a working set for data exploration

--- metrix_ml/utils/SplitData.py
+++ metrix_ml/utils/SplitData.py
@@ -1,12 +1,22 @@
-class SplitData():
-    def self():
-        pass
+#import packages
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+from sklearn.model_selection import train_test_split
+
+class SplitData(object):
+    def __init__(self, df):
+        self.df = df
     
     def split_data(df):
         X = df
         y = df['EP_success']
         X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         X_train_work = X_train.copy()
-        return X_train_work, X_train
+        return X_train_work, X_train, y_train, X_test, y_test
 
     #split_data(metrix)
+
+
