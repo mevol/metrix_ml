fc51fbdd517c8b581ec0935356c7d2395ee7e66e
mevol <melanie.vollmar@diamond.ac.uk>
2018-09-19 15:40:09 +0100

    metrix_ml/feature_corelations.py
    added several functions to analyse and plot correlations

--- metrix_ml/feature_correlations.py
+++ metrix_ml/feature_correlations.py
@@ -4,6 +4,18 @@
 # create output directories
 # start the class FeatureCorrelations
 
+
+import pandas as pd
+import os
+import numpy as np
+import csv
+from pandas.plotting import scatter_matrix
+from datetime import datetime
+from scipy.stats import pearsonr, betai
+import matplotlib.pyplot as plt
+
+
+
 ###############################################################################
 #
 #  load the data from CSV file and creating output directory
@@ -30,8 +42,21 @@ def make_output_folder(outdir):
 ###############################################################################
 
 
- class FeatureCorrelations(object):
-   def __init__(self, metrix, database, man_add, transform, prot_screen_trans):
+class FeatureCorrelations(object):
+    '''A class to help analyse the data;
+    try to identify linear correlations in the data;
+    calculate Pearson Correlation Coefficient with and without
+    p-values; create a scatter matrix; inout data must not contain
+    any strings or NaN values; also remove any columns with 
+    categorical data or transform them first; remove any text except column labels'''
+#    def __init__(self, X_train):
+#        if X_train.isnull().any().any() == True:
+#            X_train = X_train.dropna(axis=1)
+#        pass
+
+
+ 
+  def __init__(self, metrix, database, man_add, transform, prot_screen_trans):
     self.metrix=metrix
     self.database=database
     self.man_add=man_add
@@ -242,6 +267,60 @@ def make_output_folder(outdir):
     #
     ###############################################################################
 
+    def calculate_pearson_cc(X_train):
+        '''This functions calculates a simple statistics of
+        Pearson correlation coefficient'''
+        attr = list(X_train)
+        datestring = datetime.strftime(datetime.now(), '%Y%m%d_%H%M')
+        for a in attr:
+            corr_X_train = X_train.corr()        
+            with open(os.path.join(METRIX_PATH, 'linear_PearsonCC_values_'+datestring+'.txt'), 'a') as text_file:
+                corr_X_train[a].sort_values(ascending=False).to_csv(text_file)
+        text_file.close()
+    #calculate_pearson_cc(metrix)
+    
+    def print_scatter_matrix(X_train):
+        '''A function to create a scatter matrix of the data'''
+        datestring = datetime.strftime(datetime.now(), '%Y%m%d_%H%M')
+        columns = [X_train.columns]
+        for c in columns:
+            if X_train.isnull().any().any() == True:
+                X_train = X_train.dropna(axis=1)
+        attr = list(X_train)    
+        scatter_matrix(X_train[attr], figsize=(25,20))
+        plt.savefig(os.path.join(METRIX_PATH, 'linear_PearsonCC_scattermatrix'+datestring+'.png'))
+    #print_scatter_matrix(metrix)
+               
+    def corrcoef_loop(X_train):
+        attr = list(X_train)
+        rows = len(attr)
+        r = np.ones(shape=(rows, rows))
+        p = np.ones(shape=(rows, rows))
+        for i in range(rows):
+            for j in range(i+1, rows):
+                c1 = X_train[attr[i]]
+                c2 = X_train[attr[j]]
+                r_, p_ = pearsonr(c1, c2)
+                r[i, j] = r[j, i] = r_
+                p[i, j] = p[j, i] = p_
+        return r, p
+    #r,p = corrcoef_loop(metrix_num)
+
+
+
+    def write_corr(r, p, X_train):
+        datestring = datetime.strftime(datetime.now(), '%Y%m%d_%H%M')
+        with open(os.path.join(METRIX_PATH,
+                           'linear_corr_pvalues_X_database_train_work'
+                           +datestring+'.csv'), 'a') as csv_file:
+            attr = list(X_train)
+            rows = len(attr)
+            for i in range(rows):
+                for j in range(i+1, rows):
+                    csv_file.write('%s, %s, %f, %f\n' 
+                               %(attr[i], attr[j], r[i,j], p[i,j]))
+            csv_file.close()
+    #write_corr(r,p, metrix_num)
 
 
 
